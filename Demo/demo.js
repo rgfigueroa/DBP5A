b23f5f64ced7aad2b75ae8a2b94e52d13ffd00d1
rgfigueroa <roberthgfd@gmail.com>
2021-05-17 09:50:59 -0500

    Update demo.js

--- Demo/demo.js
+++ Demo/demo.js
@@ -15,10 +15,19 @@ function operar(){
 	alert(dato);
 	console.log(dato);
 	document.getElementById('valor1').style.color = "red";
+	document.getElementById('boton').style.bottom = "100px";
 
-	
 }
 function cambioColor(){
-
-	
 }
+
+function escribeFecha(){
+ 	var meses = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"];
+ 	var fecha = new Date();
+ 	console.log(fecha);
+ 	var dia = fecha.getDate();
+ 	var mesNumero = fecha.getMonth();
+ 	var mes = meses[mesNumero];
+ 	var ano = fecha.getFullYear();
+ 	document.write("Hoy es " + dia + " de " + mes + " de " + ano);
+ }
