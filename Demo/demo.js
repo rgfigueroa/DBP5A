6bda2dbcc69e9d8a651f1f8a5e18c608a5e8228a
rgfigueroa <roberthgfd@gmail.com>
2021-05-17 09:03:58 -0500

    Add files via upload

---
+++ Demo/demo.js
@@ -0,0 +1,24 @@
+alert('Hola fuera del html');
+console.log('Mensaje en consola');
+var a=4;
+let variable=5;
+
+console.log(a);
+
+let persona = {
+	nombre: 'Juan',
+	apellido:'Perez'
+}
+console.log(persona);
+function operar(){
+	let dato = document.getElementById('valor1').value;
+	alert(dato);
+	console.log(dato);
+	document.getElementById('valor1').style.color = "red";
+
+	
+}
+function cambioColor(){
+
+	
+}
